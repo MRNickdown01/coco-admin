8c367277eb920eb2befe1e1eb422936189763aae
viruop <virajrai706@gmail.com>
2022-10-27 17:45:40 +0530

    mm

--- src/Components/BrandViewLists/BrandViewList.js
+++ src/Components/BrandViewLists/BrandViewList.js
@@ -1,16 +1,41 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function BrandViewList() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function login() {
+      const response = await fetch(
+        "https://coco-backend1.herokuapp.com/getBrands",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "*/*",
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      );
+      const result = await response.json();
+      setData(result);
+      console.log(result);
+    }
+    login();
+  }, []);
+
   return (
     <div className="List of Brands">
       <div className="container-fluid">
         <h3 className="title d-flex justify-content-start mt-5 mb-5">
           List of Brands
         </h3>
+        {/* <button onClick={login}>up</button> */}
       </div>
+
       <div className="table-responsive" id="no-more-tables">
-        <table class="table align-middle mb-0 bg-white">
-          <thead class="bg-light">
+        <table className="table align-middle mb-0 bg-white">
+          <thead className="bg-light">
             <tr>
               <th>Rank</th>
               <th>Brand Name</th>
@@ -20,98 +45,44 @@ function BrandViewList() {
               <th>% for social</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td data-title="Rank">
-                <div class="d-flex align-items-center justify-content-center">
-                  <img
-                    src="https://mdbootstrap.com/img/new/avatars/8.jpg"
-                    alt=""
-                    style={{
-                      width: "45px",
-                      height: "45px",
-                    }}
-                    class="rounded-circle"
-                  />
-                </div>
-              </td>
-              <td data-title="Brand Name">
-                <p class="fw-bold mb-1">Sublime Life</p>
-              </td>
-              <td data-title="Payout Policy">
-                <p class="fw-normal mb-1">10 days</p>
-              </td>
-              <td data-title="Return Period">
-                <p class="fw-normal mb-1">10 days</p>
-              </td>
-              <td data-title="% for customer">
-                <p class="fw-normal mb-1">10 %</p>
-              </td>
-              <td data-title="% for social">
-                <p class="fw-normal mb-1">10 %</p>
-              </td>
-            </tr>
-            <tr>
-              <td data-title="Rank">
-                <div class="d-flex align-items-center justify-content-center">
-                  <img
-                    src="https://mdbootstrap.com/img/new/avatars/6.jpg"
-                    class="rounded-circle"
-                    alt=""
-                    style={{
-                      width: "45px",
-                      height: "45px",
-                    }}
-                  />
-                </div>
-              </td>
-              <td data-title="Brand Name">
-                <p class="fw-bold mb-1">Sublime Life</p>
-              </td>
-              <td data-title="Payout Policy">
-                <p class="fw-normal mb-1">10 days</p>
-              </td>
-              <td data-title="Return Period">
-                <p class="fw-normal mb-1">10 days</p>
-              </td>
-              <td data-title="% for customer">
-                <p class="fw-normal mb-1">10 %</p>
-              </td>
-              <td data-title="% for social">
-                <p class="fw-normal mb-1">10 %</p>
-              </td>
-            </tr>
-            <tr>
-              <td data-title="Rank">
-                <div class="d-flex align-items-center justify-content-center">
-                  <img
-                    src="https://mdbootstrap.com/img/new/avatars/6.jpg"
-                    class="rounded-circle"
-                    alt=""
-                    style={{
-                      width: "45px",
-                      height: "45px",
-                    }}
-                  />
-                </div>
-              </td>
-              <td data-title="Brand Name">
-                <p class="fw-bold mb-1">Sublime Life</p>
-              </td>
-              <td data-title="Payout Policy">
-                <p class="fw-normal mb-1">10 days</p>
-              </td>
-              <td data-title="Return Period">
-                <p class="fw-normal mb-1">10 days</p>
-              </td>
-              <td data-title="% for customer">
-                <p class="fw-normal mb-1">10 %</p>
-              </td>
-              <td data-title="% for social">
-                <p class="fw-normal mb-1">10 %</p>
-              </td>
-            </tr>
-          </tbody>
+          {data.map((data) => {
+            return (
+              <>
+                <tbody>
+                  <tr key={data.brandId}>
+                    <td data-title="Rank">
+                      <div class="d-flex align-items-center justify-content-center">
+                        <img
+                          src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                          alt=""
+                          style={{
+                            width: "45px",
+                            height: "45px",
+                          }}
+                          class="rounded-circle"
+                        />
+                      </div>
+                    </td>
+                    <td data-title="Brand Name">
+                      <p class="fw-bold mb-1">{data.brandName}</p>
+                    </td>
+                    <td data-title="Payout Policy">
+                      <p class="fw-normal mb-1">{data.payoutPolicy}</p>
+                    </td>
+                    <td data-title="Return Period">
+                      <p class="fw-normal mb-1">{data.returnPolicy}</p>
+                    </td>
+                    <td data-title="% for customer">
+                      <p class="fw-normal mb-1">{data.customerPercentage}</p>
+                    </td>
+                    <td data-title="% for social">
+                      <p class="fw-normal mb-1">{data.socialPercentage}</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </>
+            );
+          })}
         </table>
       </div>
     </div>
